5026ac6185ebf69076cb124521c30de5414645dd
Jurasikll <Jurasikll@yandex.ru>
2018-01-12 10:41:31 +0300

    Initial commit

---
+++ doc.go
@@ -0,0 +1,6 @@
+// elk_conn project doc.go
+
+/*
+elk_conn document
+*/
+package main
---
+++ main.go
@@ -0,0 +1,10 @@
+// elk_conn project main.go
+package main
+
+import (
+	"fmt"
+)
+
+func main() {
+	fmt.Println("Hello World!")
+}
