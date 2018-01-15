21f698a06835c9adf4bb8ca9e7d991791d4db797
Jurasikll <Jurasikll@yandex.ru>
2018-01-15 10:05:09 +0300

    0.0.2
    Add put_data

--- elk/elk.go
+++ elk/elk.go
@@ -1,17 +1,57 @@
 package elk
 
-//import (
-//	"net"
-//)
+import (
+	"bytes"
+	"encoding/json"
+	"fmt"
+	"io"
+	"io/ioutil"
+	"net/http"
+)
+
+const (
+	METHOD_POST               string = "POST"
+	METHOD_PUT                string = "PUT"
+	URL_DELIMETR              string = "/"
+	CREATE_RECORD_WITH_ID_PTR string = "%s/%s%d"
+)
 
 type Elk struct {
 	Answer_bytes []byte
 	body         string
 	elk_addres   string
+	http_client  http.Client
 }
 
 func NewElk(addres string) *Elk {
-	return &Elk{elk_addres: addres}
+	return &Elk{elk_addres: addres, http_client: http.Client{}}
+}
+
+func (e *Elk) Get_main_info() {
+	resp, _ := e.http_client.Get(e.elk_addres)
+	e.update_resp(resp.Body)
+}
+
+//data - struct, converted to json, and send as http Body; road - string, path to put data; record_id - id of record, send -1 if need add next record
+func (e *Elk) Put_data(data interface{}, road string, record_id int) {
+	var method string
+	var url string
+	if record_id > -1 {
+		url = fmt.Sprintf(CREATE_RECORD_WITH_ID_PTR, e.elk_addres, road, record_id)
+		method = METHOD_PUT
+	} else {
+		url = e.elk_addres + `/` + road
+		method = METHOD_POST
+	}
+	body, _ := json.Marshal(data)
+	fmt.Println(url)
+	req, _ := http.NewRequest(method, url, bytes.NewReader(body))
+	resp, _ := e.http_client.Do(req)
+	e.update_resp(resp.Body)
+}
+
+func (e *Elk) update_resp(body io.Reader) {
+	e.Answer_bytes, _ = ioutil.ReadAll(body)
 }
 
 func (e Elk) new_rec(path string, body string) {
--- main.go
+++ main.go
@@ -4,6 +4,7 @@ package main
 import (
 	"elk_conn/elk"
 	"fmt"
+	"time"
 
 	"github.com/BurntSushi/toml"
 )
@@ -16,7 +17,16 @@ func main() {
 	set := new(struct {
 		Elk_addres string
 	})
+	test_data := struct {
+		User    string    `json:"user"`
+		Init_id int       `json:"init_id"`
+		Is_set  bool      `json:"is_set"`
+		Time    time.Time `json:"time"`
+	}{User: "Test_user", Init_id: 125, Is_set: false, Time: time.Now()}
+	fmt.Printf("%v123\r\n", test_data)
+	test_road := "test_parent/test_child/"
 	toml.DecodeFile(CONFIG_PATH, &set)
 	elk_conn := elk.NewElk(set.Elk_addres)
-	fmt.Printf("%v\r\n", elk_conn)
+	elk_conn.Put_data(test_data, test_road, -1)
+	fmt.Printf("%v\r\n", string(elk_conn.Answer_bytes))
 }
