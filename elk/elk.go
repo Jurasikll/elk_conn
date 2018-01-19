87cb100658840d29079de60d4a6bc9646d200592
Jurasikll <Jurasikll@yandex.ru>
2018-01-19 13:14:58 +0300

    0.0.6
    End preapering

--- elk/elk.go
+++ elk/elk.go
@@ -13,8 +13,9 @@ const (
 	METHOD_POST               string = "POST"
 	METHOD_PUT                string = "PUT"
 	URL_DELIMETR              string = "/"
-	CREATE_RECORD_WITH_ID_PTR string = "%s/%s%d"
+	CREATE_RECORD_WITH_ID_PTR string = "%s/%s%s"
 	GET_DATA_URL_PTRN         string = "%s/%s_search?q=%s"
+	ADD_NEW_REC_MARK          string = "-1"
 )
 
 type Elk struct {
@@ -33,19 +34,17 @@ func (e *Elk) Get_main_info() {
 	e.update_resp(resp.Body)
 }
 
-//body - []byte convert to httpBody; road - string, path to put data; record_id - id of record, send -1 if need add next record
-func (e *Elk) Put_data(body []byte, road string, record_id int) {
-	//ToDo convert record_id to string
+//body - []byte convert to httpBody; road - string, path to put data; record_id - id of record, send elk.ADD_NEW_REC_MARK if need add next record
+func (e *Elk) Put_data(body []byte, road string, record_id string) {
 	var method string
 	var url string
-	if record_id > -1 {
+	if record_id != ADD_NEW_REC_MARK {
 		url = fmt.Sprintf(CREATE_RECORD_WITH_ID_PTR, e.elk_addres, road, record_id)
 		method = METHOD_PUT
 	} else {
 		url = e.elk_addres + `/` + road
 		method = METHOD_POST
 	}
-	//	body, _ := json.Marshal(data)
 	fmt.Println(url)
 	req, _ := http.NewRequest(method, url, bytes.NewReader(body))
 	resp, _ := e.http_client.Do(req)
@@ -62,9 +61,6 @@ func (e *Elk) Get_data(road string, query string) {
 		url := fmt.Sprintf(GET_DATA_URL_PTRN, e.elk_addres, road, query)
 		resp, _ := e.http_client.Get(url)
 		e.update_resp(resp.Body)
-		//		t := new(interface{})
-		//		json.Unmarshal(e.Answer_bytes, t)
-		//		fmt.Println(t)
 	}
 
 }
--- main.go
+++ main.go
@@ -10,6 +10,12 @@ import (
 	"github.com/BurntSushi/toml"
 )
 
+type test_answer_get struct {
+	Hits struct {
+		Total int
+	}
+}
+
 const (
 	CONFIG_PATH string = `C:\goplace\src\elk_conn\conf.ini`
 	TEST_ROAD   string = "test_parent/test_child/"
@@ -22,13 +28,16 @@ var set = new(struct {
 func main() {
 
 	toml.DecodeFile(CONFIG_PATH, &set)
-	elk_put_test()
+	elk_get_test()
 }
 
 func elk_get_test() {
 	elk_conn := elk.NewElk(set.Elk_addres)
 	elk_conn.Get_data(TEST_ROAD, "_id:AWDqn0Lv62WqGwI2oVMS")
-	//	fmt.Printf("%v\r\n", string(elk_conn.Answer_bytes))
+	ta := test_answer_get{}
+	json.Unmarshal(elk_conn.Answer_bytes, &ta)
+	fmt.Println(string(elk_conn.Answer_bytes))
+	fmt.Printf("%d\r\n", ta.Hits.Total)
 }
 
 func elk_put_test() {
@@ -42,6 +51,6 @@ func elk_put_test() {
 
 	elk_conn := elk.NewElk(set.Elk_addres)
 	body, _ := json.Marshal(test_data)
-	elk_conn.Put_data(body, TEST_ROAD, -1)
+	elk_conn.Put_data(body, TEST_ROAD, "AWEN2U5i62WqGwI2QS4d")
 	fmt.Printf("%v\r\n", string(elk_conn.Answer_bytes))
 }
